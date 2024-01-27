44378884655ec153f802b077e175910c5068dc1f
second <xrx172@gmail.com>
2024-01-27 22:19:40 +0900

    chore: 데이터 연동 세팅, db파일 추가 및 페쳐 구현

---
+++ src/types/wiki.d.ts
@@ -0,0 +1,20 @@
+interface Wiki {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+  author: {
+    id: string;
+    name: string;
+  };
+}
+
+interface NewWikiBody {
+  title: string;
+  description: string;
+  date: string;
+  author: {
+    id: string;
+    name: string;
+  };
+}
