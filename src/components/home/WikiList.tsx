5aa04eebe712cacef44ba2c41c8325f2ab656639
second <xrx172@gmail.com>
2024-01-28 22:51:10 +0900

    feat: 데이터가 없을 때의 ui추가

--- src/components/home/WikiList.tsx
+++ src/components/home/WikiList.tsx
@@ -6,10 +6,12 @@ type Props = {
 
 export const WikiList = ({ wikis }: Props) => {
   return (
-    <ul className="grid w-full h-full grid-rows-5 gap-3 ">
-      {wikis?.map((wiki: Wiki) => (
-        <WikiListItem key={wiki.id} wiki={wiki} />
-      ))}
+    <ul className="grid w-full h-full grid-rows-5 gap-3">
+      {wikis.length === 0 ? (
+        <div>아직 위키가 없어요.</div>
+      ) : (
+        wikis.map((wiki: Wiki) => <WikiListItem key={wiki.id} wiki={wiki} />)
+      )}
     </ul>
   );
 };
