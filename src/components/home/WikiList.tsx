6e2025c51319e61c73a7f04eff3dedfac7564687
second <xrx172@gmail.com>
2024-02-07 19:00:12 +0900

    fix: 옵셔널체이닝 추가

--- src/components/home/WikiList.tsx
+++ src/components/home/WikiList.tsx
@@ -7,10 +7,10 @@ type Props = {
 export const WikiList = ({ wikis }: Props) => {
   return (
     <ul className="grid w-full h-full grid-rows-5 gap-3">
-      {wikis.length === 0 ? (
+      {wikis?.length === 0 ? (
         <div>아직 위키가 없어요.</div>
       ) : (
-        wikis.map((wiki: Wiki) => <WikiListItem key={wiki.id} wiki={wiki} />)
+        wikis?.map((wiki: Wiki) => <WikiListItem key={wiki.id} wiki={wiki} />)
       )}
     </ul>
   );
